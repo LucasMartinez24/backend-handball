31754dd479720b35e0b4545ceeec0b6b063afeb8
Lucas Salvador Martinez <44769386@fi.unju.edu.ar>
2026-05-21 11:39:54 -0300

    cambio cors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://federaciondehandballjujuy.cloud",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ limit: "15mb", extended: true }));
